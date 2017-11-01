0efe355e5fae8231827faf7f93a3cd48c1851392
jenkins013 <31985262+jenkins013@users.noreply.github.com>
2017-11-01 17:54:05 +0530

    Added the automation project

---
+++ Jenkins_web_app/IISUnitTest/IISappUnitTestProject/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Remote;
+using System.Threading;
+using OpenQA.Selenium.Interactions;
+
+namespace IISappUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        InternetExplorerOptions options = new InternetExplorerOptions();
+
+        IWebDriver IEDriver = null;
+        [TestInitialize]
+        public void initialize()
+        {
+            options.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+            string idePath = @"C:\Users\nirotpalm626\Documents\Visual Studio 2015\Projects\IISappUnitTestProject\IISappUnitTestProject\IISappUnitTestProject\";
+            IEDriver = new InternetExplorerDriver(idePath, options);
+            IEDriver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IEDriver.Navigate().GoToUrl("http://localhost/Jenkins_web_app/");
+            IEDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
+            IEDriver.Manage().Window.Maximize();
+            IWebElement verifyText = IEDriver.FindElement(By.CssSelector("body > div.container.body-content > div.jumbotron > p.lead"));
+            if(verifyText.GetAttribute("innerHTML").Trim().Equals("ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS and JavaScript."))
+            {
+                Console.WriteLine("Text matched");
+            }
+            else
+            {
+                Console.WriteLine("Text Not matched");
+            }
+        }
+        [TestCleanup]
+        public void Testcleanup()
+        {
+            IEDriver.Close();
+        }
+    }
+}
